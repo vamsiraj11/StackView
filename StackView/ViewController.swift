13e9c3b89e57e95a66d06ed400da14840df03320
vamshi raj <raj-mac@vamshis-mac.local>
2016-09-30 20:27:55 +0530

    This is commit

--- StackView/ViewController.swift
+++ StackView/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var descLable: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func showDesc(_ sender: AnyObject) {
+        let  button =  sender as! UIButton
+        
+        if button.titleLabel?.text == "Show description" {
+            button.setTitle("Hide Description", for: .normal)
+            
+            UIView.animate(withDuration: 0.3, animations: {
+                self.descLable.isHidden = false
+                
+            })
+        }else{
+            button.setTitle("Show description", for: .normal)
+            
+            UIView.animate(withDuration: 0.3, animations: {
+                self.descLable.isHidden = true
+            })
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
